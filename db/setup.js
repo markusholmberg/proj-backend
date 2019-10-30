4ccc52afded3c13f4bafde7fd4c756be97e46395
Markus <holmbergmarkus1997@gmail.com>
2019-10-30 14:51:29 +0100

    Added setup file

--- db/setup.js
+++ db/setup.js
@@ -7,22 +7,18 @@ const dsn = "mongodb://localhost:27017/trade";
 async function addData() {
     const client  = await mongo.connect(dsn, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = await client.db();
+    await db.collection("items").drop();
+    await db.createCollection("items");
     const col = await db.collection("items");
-    const result = await col.insertMany(
-        {name: "Swift Spectral Tiger", quantity: 0, img: "tiger.jpg", price: 3500, history: []},
-        {name: "Magic Rooster", quantity: 0, img: "rooster.jpg", price: 1500, history: []},
-        {name: "Thunderfury", quantity: 0, img: "thunderfury.jpg", price: 1000, history: []},
-        {name: "Sulfuras", quantity: 0, img: "sulfuras.jpg", price: 1000, history: []},
-        {name: "Feldrake", quantity: 0, img: "feldrake.jpg", price: 500, history: []},
-        {name: "Tabard of Frost", quantity: 0, img: "tabard.jpg", price: 1500, history: []},
-        {name: "X-51 Nether Rocket X-TREME", quantity: 0, img: "rocket.jpg", price: 300, history: []}
-    );
-    async function(err, result) {
-        console.log("Value inserted");
-        console.log(result)
-        if (err) {
-            await console.log(err)
-        };
-        await client.close();
-    });
+    await col.insertMany([
+        {name: "Swift Spectral Tiger", quantity: 35, img: "tiger.jpg", price: 3500, history: []},
+        {name: "Magic Rooster", quantity: 55, img: "rooster.jpg", price: 1500, history: []},
+        {name: "Thunderfury", quantity: 44, img: "thunderfury.jpg", price: 1000, history: []},
+        {name: "Sulfuras", quantity: 32, img: "sulfuras.jpg", price: 1000, history: []},
+        {name: "Feldrake", quantity: 78, img: "feldrake.jpg", price: 500, history: []},
+        {name: "Tabard of Frost", quantity: 54, img: "tabard.jpg", price: 1500, history: []},
+        {name: "X-51 Nether Rocket X-TREME", quantity: 98, img: "rocket.jpg", price: 300, history: []}
+    ]);
+    await client.close();
 }
+addData();
