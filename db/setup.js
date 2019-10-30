899f7790a129b871274b9cb2867397444fc9799c
Markus <holmbergmarkus1997@gmail.com>
2019-10-30 14:30:58 +0100

    setup fixeroni

--- db/setup.js
+++ db/setup.js
@@ -8,7 +8,7 @@ async function addData() {
     const client  = await mongo.connect(dsn, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = await client.db();
     const col = await db.collection("items");
-    const result = await col.insertMany({
+    const result = await col.insertMany(
         {name: "Swift Spectral Tiger", quantity: 0, img: "tiger.jpg", price: 3500, history: []},
         {name: "Magic Rooster", quantity: 0, img: "rooster.jpg", price: 1500, history: []},
         {name: "Thunderfury", quantity: 0, img: "thunderfury.jpg", price: 1000, history: []},
@@ -16,7 +16,6 @@ async function addData() {
         {name: "Feldrake", quantity: 0, img: "feldrake.jpg", price: 500, history: []},
         {name: "Tabard of Frost", quantity: 0, img: "tabard.jpg", price: 1500, history: []},
         {name: "X-51 Nether Rocket X-TREME", quantity: 0, img: "rocket.jpg", price: 300, history: []},
-    },
     async function(err, result) {
         console.log("Value inserted");
         console.log(result)
